175318522556a2d455b120580794a3366a52dcf9
Josua Manullang <j7.manullang@gmail.com>
2021-08-23 04:29:51 +0700

    Deploy

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# Satu - HTML and CSS
+# Satu - HTML & CSS
+
+### Intro
+
+---
+
+This website was created to sharpen basic skills and knowledge such as how a
+page works on a web. Through this web, I can master many things, starting from
+the process of how a website will be displayed until it can be accessed directly
+through a browser.
+
+Also this website is a side project. There are many things that I can develop
+and make better after this.
+
+## The Goal
+
+---
+
+-   [x] Designing with Figma
+-   [x] Build it from basic (everything from Vanilla)
+-   [ ] Add Modern CSS (Flexbox + Grid)
+-   [ ] Add CSS Preprocessor (SASS)
+-   [ ] Deploy to Digital Ocean Droplet
+-   [ ] Add more interactivity with JavaScript
+
+## What do I use ?
+
+---
+
+-   VS Code
+-   Chrome
+-   HTML
+-   CSS
+-   Git
+-   Ubuntu
+-   Figma
+-   Resource
+    1. [unsplash](https://unsplash.com/)
+    2. [wikipedia](https://www.wikipedia.org/)
+    3. [flaticon](https://www.flaticon.com/)
+
+## Start simple, make it work, and develop it
+
+---
+
+Start with simple things, over time fulfill all the goals to be achieved.
+
+## Conclusion
+
+---
+
+**Start small, Go Solo, Very Excited :grin:**
---
+++ global.js
@@ -0,0 +1,38 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+
+var modalNoButton = document.querySelector('.modal-action-negative');
+var selectAvailable = document.querySelectorAll('.available-packages button');
+var selectPackage = document.querySelectorAll('.detail-applies button');
+
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNavigations = document.querySelector('.mobile-navigations');
+
+for (var i = 0; i < selectAvailable.length; i++) {
+    selectAvailable[i].addEventListener('click', function () {
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    });
+}
+
+for (var i = 0; i < selectPackage.length; i++) {
+    selectPackage[i].addEventListener('click', function () {
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    });
+}
+toggleButton.addEventListener('click', function () {
+    backdrop.style.display = 'block';
+    mobileNavigations.style.display = 'block';
+});
+
+backdrop.addEventListener('click', closeModal);
+modalNoButton.addEventListener('click', closeModal);
+
+function closeModal() {
+    if (modal) {
+        modal.style.display = 'none';
+    }
+    mobileNavigations.style.display = 'none';
+    backdrop.style.display = 'none';
+}
